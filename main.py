995b34482d899aeef93bb405bde52d8939cae159
Ashot Abazyan <aabazyan@wv00602.local>
2022-09-05 16:45:41 +0300

    Added file download func

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+from pathlib import Path
+
+
+def file_download(filename, url):
+    response = requests.get(url)
+    response.raise_for_status()
+    Path('images').mkdir(parents=True, exist_ok=True)
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+
+def main():
+    filename = 'hubble.jpeg'
+    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    file_download(filename, url)
+
+
+if __name__ == '__main__':
+    main()
