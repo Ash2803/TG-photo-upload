d67045d6abef034490c56935f5705e070c327505
Ashot Abazyan <aabazyan@wv00602.local>
2022-09-06 17:27:55 +0400

    Added fetch_spacex_last_luanch func

--- main.py
+++ main.py
@@ -2,18 +2,31 @@ import requests
 from pathlib import Path
 
 
-def file_download(filename, url):
-    response = requests.get(url)
+def file_download(path, url):
+    """Download files to user created directory"""
+    headers = {
+        'User-Agent': 'CoolBot/0.0 (https://example.org/coolbot/; coolbot@example.org)'
+    }
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
-    Path('images').mkdir(parents=True, exist_ok=True)
-    with open(f'images/{filename}', 'wb') as file:
+    Path(path).mkdir(parents=True, exist_ok=True)
+    with open(f'{path}/hubble.jpeg', 'wb') as file:
         file.write(response.content)
 
 
+def fetch_spacex_last_launch(launch_id):
+    """Get photos of the last SpaceX rocket launch"""
+    response = requests.get(f'https://api.spacexdata.com/v5/launches/{launch_id}')
+    response.raise_for_status()
+    return response.json()['links']['flickr']['original']
+
+
 def main():
-    filename = 'hubble.jpeg'
+    path = input()
     url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    file_download(filename, url)
+    file_download(path, url)
+    launch_id = input()
+    print(fetch_spacex_last_launch(launch_id))
 
 
 if __name__ == '__main__':
