830f180bb447b04406c4349bd5e3ed98dd3260c4
Ash2803 <ashot.93@mail.ru>
2022-09-07 21:44:39 +0400

    Added for loop for downloading photos in spacex func

--- main.py
+++ main.py
@@ -4,29 +4,32 @@ from pathlib import Path
 
 def file_download(path, url):
     """Download files to user created directory"""
-    headers = {
-        'User-Agent': 'CoolBot/0.0 (https://example.org/coolbot/; coolbot@example.org)'
-    }
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     response.raise_for_status()
     Path(path).mkdir(parents=True, exist_ok=True)
     with open(f'{path}/hubble.jpeg', 'wb') as file:
         file.write(response.content)
 
 
-def fetch_spacex_last_launch(launch_id):
+def fetch_spacex_last_launch(launch_id, path):
     """Get photos of the last SpaceX rocket launch"""
     response = requests.get(f'https://api.spacexdata.com/v5/launches/{launch_id}')
     response.raise_for_status()
-    return response.json()['links']['flickr']['original']
+    urls = response.json()['links']['flickr']['original']
+    Path(path).mkdir(parents=True, exist_ok=True)
+    for url_number, url in enumerate(urls):
+        image = requests.get(url)
+        image.raise_for_status()
+        with open(f'{path}/spacex_{url_number}.jpeg', 'wb') as file:
+            file.write(image.content)
 
 
 def main():
-    path = input()
-    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    file_download(path, url)
-    launch_id = input()
-    print(fetch_spacex_last_launch(launch_id))
+    # url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    # file_download(path, url)
+    launch_id = input('Enter launch id: ')
+    path = input('Enter download path: ')
+    fetch_spacex_last_launch(launch_id, path)
 
 
 if __name__ == '__main__':
