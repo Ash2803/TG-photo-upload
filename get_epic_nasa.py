475859e78bed88083bebc92ac9dda19866977947
Ash2803 <ashot.93@mail.ru>
2022-09-15 19:25:21 +0400

    Updated get_epic_nasa script

--- get_epic_nasa.py
+++ get_epic_nasa.py
@@ -6,14 +6,14 @@ from dotenv import load_dotenv
 from get_format import get_format
 
 
-def get_epic_nasa(token, path):
+def get_epic_nasa(token):
     """Downloading EPIC NASA photos"""
     param = {
         'api_key': token
     }
     response = requests.get('https://api.nasa.gov/EPIC/api/natural?api_key=DEMO_KEY', params=param)
     response.raise_for_status()
-    Path(path).mkdir(parents=True, exist_ok=True)
+    Path('images').mkdir(parents=True, exist_ok=True)
     for link_number, link in enumerate(response.json(), start=1):
         image_name = link['image']
         image_date = link['date'].split(" ")[0].replace('-', '/')
@@ -21,15 +21,14 @@ def get_epic_nasa(token, path):
         file_format = get_format(image_url)
         image = requests.get(image_url)
         image.raise_for_status()
-        with open(f'{path}/nasa_epic_photo{link_number}{file_format}', 'wb') as file:
+        with open(f'images/nasa_epic_photo{link_number}{file_format}', 'wb') as file:
             file.write(image.content)
 
 
 def main():
-    path = input('Enter download path: ')
     load_dotenv()
     nasa_apikey = os.getenv('NASA_API_KEY')
-    get_epic_nasa(nasa_apikey, path)
+    get_epic_nasa(nasa_apikey)
 
 
 if __name__ == '__main__':
