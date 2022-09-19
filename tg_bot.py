aa4a9de926ac5335d68e688f592e06f7d1e032b6
Ash2803 <ashot.93@mail.ru>
2022-09-20 00:15:50 +0400

    Added argparse for posting intervals

--- tg_bot.py
+++ tg_bot.py
@@ -1,20 +1,37 @@
+import argparse
 import os
 import random
+import time
 
 import telegram
 from dotenv import load_dotenv
+
+
+def posting_files(chat_id, tg_token, posting_time):
+    bot = telegram.Bot(token=tg_token)
+    files = os.listdir('images')
+    random.shuffle(files)
+    while True:
+        for file_name in files:
+            time.sleep(int(posting_time))
+            with open(f'images/{file_name}', 'rb') as file:
+                bot.send_photo(chat_id=chat_id, photo=file)
 
 
 def main():
     load_dotenv()
     tg_token = os.getenv('TG_BOT_TOKEN')
     chat_id = os.getenv('TG_CHAT_ID')
-    bot = telegram.Bot(token=tg_token)
-    bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
-    file_names = os.listdir('images')
-    while True:
-        with open(f'images/{random.choice(file_names)}', 'rb') as file:
-            bot.send_photo(chat_id=chat_id, photo=file)
+    posting_time = os.getenv('POSTING_TIME')
+    parser = argparse.ArgumentParser(
+        description='Публикует фото в телеграм бота'
+    )
+    parser.add_argument('-t', '--posting_time', help='Кол-во времени задержки между публикациями', type=int)
+    args = parser.parse_args()
+    if args.posting_time:
+        posting_files(chat_id, tg_token, args.posting_time)
+    else:
+        posting_files(chat_id, tg_token, posting_time)
 
 
 if __name__ == '__main__':
