398dd3a2e0c8a5a4140c42ec6edc4a5f5fa30a78
Ash2803 <ashot.93@mail.ru>
2022-09-22 02:58:03 +0400

    Fixed date formatting in get_epic_nasa.py

--- get_epic_nasa.py
+++ get_epic_nasa.py
@@ -2,6 +2,7 @@ import os
 import requests
 from dotenv import load_dotenv
 from images_download import fetch_images
+from datetime import datetime
 
 
 def get_epic_nasa(token):
@@ -14,7 +15,8 @@ def get_epic_nasa(token):
     urls = []
     for link_number, link in enumerate(response.json(), start=1):
         image_name = link['image']
-        image_date = link['date'].split(" ")[0].replace('-', '/')
+        a_date = datetime.fromisoformat(link['date'])
+        image_date = f'{a_date.year}\{a_date.month}\{a_date.day}'
         image_url = f'https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png?api_key={token}'
         urls.append(image_url)
         fetch_images(urls)
