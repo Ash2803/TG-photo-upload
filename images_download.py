7207c0f8add908ac1b0126b8dadb16673ed730d9
Ash2803 <ashot.93@mail.ru>
2022-09-22 02:02:56 +0400

    Fixed fetcch_images function, downloading path

--- images_download.py
+++ images_download.py
@@ -6,12 +6,13 @@ from get_format import get_format
 
 
 def fetch_images(urls):
-    Path('images').mkdir(parents=True, exist_ok=True)
+    image_dir_path = Path('images')
+    image_dir_path.mkdir(parents=True, exist_ok=True)
     for url_number, url in enumerate(urls):
         file_format = get_format(url)
         image = requests.get(url)
         image.raise_for_status()
-        with open(f'images/photo_{url_number}{file_format}', 'wb') as file:
+        with open(image_dir_path / f'photo_{url_number}{file_format}', 'wb') as file:
             file.write(image.content)
 
 
