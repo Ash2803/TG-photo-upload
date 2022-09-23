075431a2629e5d4f1dc216c86566a51bcdcafcc6
Ash2803 <ashot.93@mail.ru>
2022-09-23 12:29:34 +0400

    Fixed GET request in fetch_images func and url in get_epic_nasa func

--- get_epic_nasa.py
+++ get_epic_nasa.py
@@ -2,7 +2,7 @@ import os
 import requests
 from dotenv import load_dotenv
 from images_download import fetch_images
-from datetime import datetime
+import datetime
 
 
 def get_epic_nasa(token):
@@ -10,16 +10,16 @@ def get_epic_nasa(token):
     params = {
         'api_key': token
     }
-    response = requests.get('https://api.nasa.gov/EPIC/api/natural', params=param)
+    response = requests.get('https://api.nasa.gov/EPIC/api/natural', params=params)
     response.raise_for_status()
     urls = []
     for link_number, link in enumerate(response.json(), start=1):
         image_name = link['image']
-        a_date = datetime.fromisoformat(link['date'])
-        image_date = f'{a_date.year}\{a_date.month}\{a_date.day}'
-        image_url = f'https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png?api_key={token}'
+        a_date = datetime.datetime.fromisoformat(link['date'])
+        image_date = f"{a_date.year}/0{a_date.month}/{a_date.day}"
+        image_url = f'https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png'
         urls.append(image_url)
-        fetch_images(urls)
+        fetch_images(urls, params)
 
 
 def main():
--- images_download.py
+++ images_download.py
@@ -5,12 +5,12 @@ from pathlib import Path
 from get_format import get_format
 
 
-def fetch_images(urls):
+def fetch_images(urls, params):
     image_dir_path = Path('images')
     image_dir_path.mkdir(parents=True, exist_ok=True)
     for url_number, url in enumerate(urls):
         file_format = get_format(url)
-        image = requests.get(url)
+        image = requests.get(url, params=params)
         image.raise_for_status()
         with open(image_dir_path / f'photo_{url_number}{file_format}', 'wb') as file:
             file.write(image.content)
